354dc835e2ab2a3e62e961a75c46c9a9939a8408
won237bin9935-dot <won237bin9935@gmail.com>
2026-05-05 13:57:47 +0800

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -17,7 +17,7 @@ const HERO_IMAGES = [
   { src: "/hero-sale-4.png", position: "55% center" },
   { src: "/hero-sale-5.jpg", position: "52% center" },
 
-  { src: "/hero-sale-7.png",  position: "80% center" } // 新增最後一張
+  { src: "/hero-sale-7.png",  position: "80% 60%" } // 新增最後一張
 ];
 
 function useStorage(key, defaultVal) {
